25f59c2e3c8a55564f39e25fa408ea80b8f9bfa0
Yixin Gong <sam19931225@gmail.com>
2022-05-09 17:54:36 -0400

    initial

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 //Our code here
 
+
+let localDb = [];
 //Getting data using fetch
 fetch(`https://date.nager.at/api/v2/publicholidays/2023/US`)
 .then(res => res.json())
-.then(data => data.forEach((item)=>console.log(item)))
+.then(data => data.forEach((item)=>addItemToDb(item)))
+
+function addItemToDb(item){
+    localDb.push() = item
+    console.log(localDb)
+}
