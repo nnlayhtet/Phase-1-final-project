2386c3f18643d14fe044e7e94d221bc50379bd5b
nyinyilayhtet <nnlayhtet@gmail.com>
2022-05-09 17:31:09 -0400

    data

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 //Our code here
+
+//Getting data using fetch
+fetch(`https://date.nager.at/api/v2/publicholidays/2023/US`)
+.then(res => res.json())
+.then(data => data.forEach((item)=>console.log(item)))
