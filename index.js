0c60896f080d61cbd91fe41984cd149832cf7c93
nyinyilayhtet <nnlayhtet@gmail.com>
2022-05-10 10:09:51 -0400

    added init db.json data

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 //Our code here
-
+let db = []
 //Getting data using fetch
-fetch(`https://date.nager.at/api/v2/publicholidays/2023/US`)
-.then(res => res.json())
-.then(data => data.forEach((item)=>console.log(item)))
+// fetch(`https://date.nager.at/api/v2/publicholidays/2023/US`)
+// .then(res => res.json())
+// .then(data => data.map((item)=>{
+//     db.push(item)
+
+// }))
+// console.log(db)
+
+// fetch(`https://date.nager.at/api/v2/publicholidays/2023/US`)
+// .then(res=> {
+//     fetch
+//      )
