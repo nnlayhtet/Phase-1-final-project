ac6201f96a61ab3b2b977e575e40c356bb368fb4
nyinyilayhtet <nnlayhtet@gmail.com>
2022-05-11 17:21:53 -0400

    functions

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 //Our code here
 
+//Declare global variables
 let phList = document.getElementById('ph-list')
 let showHoliday = document.getElementById('show-holiday')
+let newHolidayForm = document.getElementById('new-holiday-form')
+let selectCountry = document.getElementById('select-country')
+let selectYear = document.getElementById('select-year')
+let goButton = document.getElementById('go-button')
 
 //Getting data using fetch and render on page
-function fetchAndRender(){
-    fetch(`https://date.nager.at/api/v2/publicholidays/2023/US`)
+fetchAndRender()
+function fetchAndRender(country='US', year='2022'){
+    fetch(`https://date.nager.at/api/v2/publicholidays/${year}/${country}`)
     .then(res => res.json())
     .then(data => renderHolidayList(data))
 }
+
 function renderHolidayList(data){
+    phList.innerHTML = ""
+    showHoliday.innerHTML = ""
     data.map(element=>{
         let holidayName = document.createElement('li')
-        holidayName.textContent = element.name
-        //console.log(holidayName)
+        holidayName.textContent = element.name.toUpperCase()
+        holidayName.style.color = 'blue'
+        holidayName.style.textDecoration = 'underline'
         phList.append(holidayName)
-
         holidayName.addEventListener('click',()=>{
             renderHoliday(element)
-            
         })
     })
-
 }
-fetchAndRender()
 
 function renderHoliday(element){
     showHoliday.innerHTML = ""
@@ -33,39 +39,35 @@ function renderHoliday(element){
     dateDisplay.textContent = `Date : ${element.date}` 
     let type = document.createElement('p')
     type.textContent = `Type of holiday : ${element.type}`
-    let likeButton = document.createElement('button')
-    likeButton.textContent = 'Like'
-    showHoliday.append(nameDisplay,dateDisplay,type,likeButton)
-
-
-
-
+    // // // let likeButton = document.createElement('button')
+    // // // likeButton.textContent = 'Like'
+    showHoliday.append(nameDisplay,dateDisplay,type)
 }
+let country = 'US';
+selectCountry.addEventListener('change',(e)=>{
+    country = e.target.value
+})
+
+let year = '2022';
+selectYear.addEventListener('change',(e)=>{
+    year = e.target.value
+})
 
 
+goButton.addEventListener('click', function(){
+    fetchAndRender(country, year) 
+})
 
-// // A sample new holiday post
-// let newPost = {    "date": "2023-12-25",
-// "localName": "My Birth-Day",
-// "name": "Birth Day",
-// "countryCode": "US",
-// "fixed": false,
-// "global": true,
-// "counties": null,
-// "launchYear": null,
-// "type": "Local"}
+newHolidayForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    let newNameTag = document.createElement('li')
+    newNameTag.textContent = `${e.target.name.value} (new)`
+    newNameTag.style.color = 'blue'
+    newNameTag.style.textDecoration = 'underline'
+    phList.append(newNameTag)
+    let newObj ={'name':`${e.target.name.value}`, 'date':`${e.target.date.value}`, 'type':`${e.target.type.value}`}
+    newNameTag.addEventListener('click',()=>renderHoliday(newObj))
+    newHolidayForm.reset()
 
+})
 
-// // To Post a new local holiday 
-// const postContent = (newPost) =>{
-//     return fetch(`http://localhost:3000/2023`,{
-//     method:'POST',
-//     headers: {
-//         'Content-Type' : 'application/json'
-//     },
-//     body: JSON.stringify(newPost)
-
-// })
-// .then(res => res.json())
-// }
-//.then(data => console.log(data))
